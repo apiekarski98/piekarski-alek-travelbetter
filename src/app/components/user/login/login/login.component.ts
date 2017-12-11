1220138a84ebafdc11f4b8e4a3fc240e442b56ae
Alek Piekarski <piekarski.a@husky.neu.edu>
2017-12-10 21:46:37 -0500

    Added login functionality.

--- src/app/components/user/login/login/login.component.ts
+++ src/app/components/user/login/login/login.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {Router} from '@angular/router';
+import {UserService} from "../../../../services/user.service.client";
+import {NgForm} from '@angular/forms';
+import {SharedService} from "../../../../services/shared.service.client";
 
 @Component({
   selector: 'app-login',
@@ -6,10 +10,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  @ViewChild('f') loginForm: NgForm;
 
-  constructor() { }
+  username: String;
+  password: String;
+  errorFlag: boolean;
+  errorMsg = 'Invalid username or password!';
+
+
+  constructor(private sharedService: SharedService,
+              private userService: UserService,
+              private router: Router) {
+  }
 
   ngOnInit() {
+  }
+
+  login() {
+    this.username = this.loginForm.value.username;
+    this.password = this.loginForm.value.password;
+
+    this.userService.login(this.username, this.password)
+      .subscribe((user) => {
+        this.sharedService.user = user;
+        this.router.navigate(['/profile']);
+      });
   }
 
 }
