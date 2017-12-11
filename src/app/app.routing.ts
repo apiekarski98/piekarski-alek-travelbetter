a30dce744a5a6173bf5d650f143b02b0d316f663
Alek Piekarski <piekarski.a@husky.neu.edu>
2017-12-10 19:22:54 -0500

    Updated routing to include new files.

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -7,10 +7,12 @@ import {ModuleWithProviders} from '@angular/core';
 import {TestComponent} from './components/test/test.component';
 import {WebsiteListComponent} from './components/website/website-list/website-list.component';
 import {SearchComponent} from "./components/search/search.component";
+import {SearchDetailsComponent} from "./components/search/search-details/search-details/search-details.component";
 
 const APP_ROUTES: Routes = [
   {path: '', component: SearchComponent},
   {path: 'search', component: SearchComponent},
+  {path: 'search/:countryName', component: SearchDetailsComponent},
   {path: 'test', component: TestComponent},
   {path: 'website', component: WebsiteListComponent}
 ];
