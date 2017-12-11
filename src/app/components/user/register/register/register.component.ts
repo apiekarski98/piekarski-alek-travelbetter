415df55c79f1175c53ea62769e3457e4b7a79408
Alek Piekarski <piekarski.a@husky.neu.edu>
2017-12-10 21:46:15 -0500

    Added register functionality.

--- src/app/components/user/register/register/register.component.ts
+++ src/app/components/user/register/register/register.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {Router} from '@angular/router';
+import {NgForm} from '@angular/forms';
+import {SharedService} from "../../../../services/shared.service.client";
+import {UserService} from "../../../../services/user.service.client";
 
 @Component({
   selector: 'app-register',
@@ -6,10 +10,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  @ViewChild('f') registerForm: NgForm;
 
-  constructor() { }
+  username: String;
+  password: String;
+  verifyPassword: String;
+  errorFlag: boolean;
+  errorMsg = 'Passwords do not match!';
+
+  constructor(private sharedService: SharedService,
+              private userService: UserService,
+              private router: Router) {
+
+  }
 
   ngOnInit() {
+  }
+
+  register() {
+    this.username = this.registerForm.value.username;
+    this.password = this.registerForm.value.password;
+    this.verifyPassword = this.registerForm.value.verifyPassword;
+    if (this.password === this.verifyPassword) {
+      this.userService.register(this.username, this.password)
+        .subscribe((user) => {
+          this.sharedService.user = user;
+          this.router.navigate(['/profile']);
+        });
+    } else {
+      this.errorFlag = true;
+    }
   }
 
 }
